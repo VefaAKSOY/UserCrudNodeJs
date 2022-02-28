a3487e2923a81365a68fba7aeb67fafcede14d10
Vefa AKSOY <bayramvefa.aksoy@gmail.com>
2022-02-28 16:02:47 +0300

    Error Handling is done

--- Backend/middleware/error/errorhandling.js
+++ Backend/middleware/error/errorhandling.js
@@ -1,14 +1,16 @@
+const writeFile = require('../../logging/logfile');
 const ApiError = require('./apierror')
 
-function apiErrorHandler(err, req, res, next){
+function apiErrorHandler(err, req, res, next) {
     console.log(err)
-    if(err instanceof ApiError){
+    if (err instanceof ApiError) {
+        
+        writeFile("Error Code: "+err.code + " " + err.message);
         res.status(err.code).json(err.message);
-       
         return;
     }
-    
-    
+
+
 }
 
 module.exports = apiErrorHandler;
